2493bb449d7ce05ee9eecc32112c6c653c489ec7
MOIS3Y <stepan@zhukovsky.me>
2023-02-04 19:09:52 +0900

    Simple test but working nixos configuration

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+	config.allowUnfree = true;
+      };
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        honor = lib.nixosSystem {
+          inherit system;
+	  modules = [
+            ./system/configuration.nix
+	  ];
+	};
+      };
+    }; 
+}
