91bcb69774181f34d58269de6ed30bbe9a18a079
MOIS3Y <stepan@zhukovsky.me>
2024-01-06 18:30:09 +0900

    Fix: vps-allsave boot GRUB device path

--- hosts/vps-allsave/boot/loader.nix
+++ hosts/vps-allsave/boot/loader.nix
@@ -5,7 +5,7 @@
 
   boot.loader = {
     grub = {
-      device = "/dev/ata-HP_SSD_S650_120GB_HASA12100101184";
+      device = "/dev/disk/by-id/ata-HP_SSD_S650_120GB_HASA12100101184";
       configurationLimit = 7;
     };
   };
