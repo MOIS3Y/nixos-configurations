1cb43d73054a93dd74bdbf851e83bedceac4d09b
MOIS3Y <stepan@zhukovsky.me>
2024-03-27 21:09:32 +0900

    Update: new common playmoth theme

--- hosts/msi-z390-a-pro/boot/plymouth.nix
+++ hosts/msi-z390-a-pro/boot/plymouth.nix
@@ -6,8 +6,9 @@
   boot.plymouth = {
   enable = true;
   themePackages = [
-    (pkgs.catppuccin-plymouth.override { variant = "mocha"; })
+    # (pkgs.catppuccin-plymouth.override { variant = "mocha"; })
+    pkgs.adi1090x-plymouth-themes
   ];
-  theme = "catppuccin-mocha";
+  theme = "connect";
   };
 }
