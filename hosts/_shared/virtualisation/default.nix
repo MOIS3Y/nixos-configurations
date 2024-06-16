630e3b11dfde415140344335748cb2646b16d9b3
MOIS3Y <stepan@zhukovsky.me>
2024-06-16 13:45:38 +0900

    Update: disable autostart virtualization services

--- hosts/_shared/virtualisation/default.nix
+++ hosts/_shared/virtualisation/default.nix
@@ -2,10 +2,14 @@
 # ▀▄▀ █ █▀▄ ░█░ █▄█ █▀█ █▄▄ █ █▄ █▀█ ░█░ █ █▄█ █░▀█ ▄
 # -- -- -- -- -- -- -- -- -- --
 
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
 
   imports = [
     ./docker.nix
     ./libvirtd.nix
   ];
+  # don't autostart services: 
+  systemd.services.libvirtd.wantedBy = lib.mkForce [];
+  systemd.services.libvirt-guests.wantedBy = lib.mkForce [];
+  systemd.services.docker.wantedBy = lib.mkForce [];
 }
