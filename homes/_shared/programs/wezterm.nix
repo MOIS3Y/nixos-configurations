978d6e9918c15ca6cb6b25f4825ea4c614c20ce6
MOIS3Y <stepan@zhukovsky.me>
2024-06-17 10:52:23 +0900

    Update: wezterm follow config color scheme

--- homes/_shared/programs/wezterm.nix
+++ homes/_shared/programs/wezterm.nix
@@ -145,49 +145,61 @@
           },
         },
 
-        -- Catppuccin Mocha Colorscheme
+        -- ${config.colorScheme.name} Colorscheme
         bold_brightens_ansi_colors = true,
         colors = {
-          foreground = "#cdd6f4",
-          background = "#11111b",  -- #1e1e2e #11111b crust
-          cursor_bg = "#cdd6f4",
-          cursor_fg = "#cdd6f4",
-          cursor_border = "#6c7086",
-          selection_fg = "#1e1e2e",
-          selection_bg = "#cdd6f4",
-          scrollbar_thumb = "#cdd6f4",
-          split = "#11111b",
+          foreground = "#${config.colorScheme.palette.base05}",
+          background = "#${config.colorScheme.palette.base00}",
+          cursor_bg = "#${config.colorScheme.palette.base05}",
+          cursor_fg = "#${config.colorScheme.palette.base05}",
+          cursor_border = "#${config.colorScheme.palette.base03}",
+          selection_fg = "#${config.colorScheme.palette.base01}",
+          selection_bg = "#${config.colorScheme.palette.base05}",
+          scrollbar_thumb = "#${config.colorScheme.palette.base05}",
+          split = "#${config.colorScheme.palette.base00}",
           ansi = {
-            "#1e1e2e",
-            "#f38ba8",
-            "#a6e3a1",
-            "#fab387",
-            "#89b4fa",
-            "#cba6f7",
-            "#94e2d5",
-            "#e4e6e7"
+            "#${config.colorScheme.palette.base01}",
+            "#${config.colorScheme.palette.base08}",
+            "#${config.colorScheme.palette.base0B}",
+            "#${config.colorScheme.palette.base09}",
+            "#${config.colorScheme.palette.base0D}",
+            "#${config.colorScheme.palette.base0E}",
+            "#${config.colorScheme.palette.base0C}",
+            "#${config.colorScheme.palette.base05}"
           },
           brights = {
-            "#6c7086",
-            "#f38ba8",
-            "#a6e3a1",
-            "#fab387",
-            "#89b4fa",
-            "#cba6f7",
-            "#94e2d5",
-            "#f2f4f5"
+            "#${config.colorScheme.palette.base04}",
+            "#${config.colorScheme.palette.base08}",
+            "#${config.colorScheme.palette.base0B}",
+            "#${config.colorScheme.palette.base09}",
+            "#${config.colorScheme.palette.base0D}",
+            "#${config.colorScheme.palette.base0E}",
+            "#${config.colorScheme.palette.base0C}",
+            "#${config.colorScheme.palette.base05}"
           },
-          indexed = { [136] = "#cdd6f4" },
+          indexed = { [136] = "#${config.colorScheme.palette.base05}" },
           tab_bar = {
             active_tab = {
-              bg_color = "#1e1e2e",
-              fg_color = "#cdd6f4",
+              bg_color = "#${config.colorScheme.palette.base01}",
+              fg_color = "#${config.colorScheme.palette.base05}",
               italic = true,
             },
-            inactive_tab = { bg_color = "#11111b", fg_color = "#1e1e2e" },
-            inactive_tab_hover = { bg_color = "#1e1e2e", fg_color = "#cdd6f4" },
-            new_tab = { bg_color = "#1e1e2e", fg_color = "#cdd6f4" },
-            new_tab_hover = { bg_color = "#89b4fa", fg_color = "#11111b" },
+            inactive_tab = { 
+              bg_color = "#${config.colorScheme.palette.base00}",
+              fg_color = "#${config.colorScheme.palette.base01}"
+            },
+            inactive_tab_hover = {
+              bg_color = "#${config.colorScheme.palette.base01}",
+              fg_color = "#${config.colorScheme.palette.base05}"
+            },
+            new_tab = {
+              bg_color = "#${config.colorScheme.palette.base01}",
+              fg_color = "#${config.colorScheme.palette.base05}"
+            },
+            new_tab_hover = {
+              bg_color = "#${config.colorScheme.palette.base0D}",
+              fg_color = "#${config.colorScheme.palette.base00}"
+            },
           },
         },
 
@@ -211,7 +223,7 @@
         window_background_opacity = 0.8,
         window_close_confirmation = "NeverPrompt",
         window_frame = {
-          active_titlebar_bg = "#11111b",
+          active_titlebar_bg = "#${config.colorScheme.palette.base00}",
           font = font_with_fallback(font_name,
           { bold = true })
         },
