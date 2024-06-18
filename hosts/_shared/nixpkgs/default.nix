7f2f01959fc138963aa91cb3366bd31108025b4b
MOIS3Y <stepan@zhukovsky.me>
2024-06-18 23:30:21 +0900

    Update: sddm follow config theme and colors

--- hosts/_shared/nixpkgs/default.nix
+++ hosts/_shared/nixpkgs/default.nix
@@ -13,11 +13,15 @@
         i3lock-run = inputs.i3lock-color-wrapper.packages."${pkgs.system}".i3lock-color-wrapper;
         xidlehook-caffeine = inputs.xidlehook-caffeine.packages."${pkgs.system}".xidlehook-caffeine;
         aladdin4nix = inputs.aladdin4nix.packages."${pkgs.system}".aladdin4nix;
+        assets4nix = inputs.assets4nix.packages."${pkgs.system}".assets4nix;
+        sddm-sugar-candy = inputs.sddmSugarCandy4Nix.packages."${pkgs.system}".sddm-sugar-candy;
       in {
         extrapkgs = {
           inherit i3lock-run;
           inherit xidlehook-caffeine;
           inherit aladdin4nix;
+          inherit assets4nix;
+          inherit sddm-sugar-candy;
         };
       })
     ];
--- hosts/_shared/services/displayManager.nix
+++ hosts/_shared/services/displayManager.nix
@@ -2,10 +2,7 @@
 # █▄▀ █ ▄█ █▀▀ █▄▄ █▀█ ░█░   █░▀░█ █▀█ █░▀█ █▀█ █▄█ ██▄ █▀▄ ▄
 # -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-{ config, pkgs, ... }: 
-  let
-    extra-pkgs = pkgs.callPackage ../extrapkgs {};
-  in {
+{ config, pkgs, ... }: {
   services = {
     displayManager = {
       enable = true;
@@ -18,7 +15,22 @@
           libsForQt5.qt5.qtgraphicaleffects
         ];
         autoNumlock = true;
-        theme = "${extra-pkgs.sugar-candy}";
+        theme = ''${
+          pkgs.extrapkgs.sddm-sugar-candy.override {
+            settings = {
+              Background = ''${pkgs.extrapkgs.assets4nix}/share/wallpapers/nixos/${config.colorScheme.name}.png'';
+              ScreenWidth = "1920";
+              ScreenHeight = "1080";
+              MainColor = "#${config.colorScheme.palette.base05}";
+              AccentColor = "#${config.colorScheme.palette.base0D}";
+              BackgroundColor = "#${config.colorScheme.palette.base02}";
+              OverrideLoginButtonTextColor = "#${config.colorScheme.palette.base01}";
+              Font = "Ubuntu";
+              DateFormat = "dddd, d MMMM yyyy";
+              HeaderText = "NixOS";
+            };
+          }
+        }'';
         settings = {
           Theme = { CursorTheme = "catppuccin-mocha-blue-cursors"; };
         };
