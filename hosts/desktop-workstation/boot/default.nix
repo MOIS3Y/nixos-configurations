8769541d96c4879db307d35a3d292dfd5e346b04
MOIS3Y <stepan@zhukovsky.me>
2024-06-22 19:35:10 +0900

    Add: dcci_backlight support

--- hosts/desktop-workstation/boot/default.nix
+++ hosts/desktop-workstation/boot/default.nix
@@ -3,13 +3,17 @@
 # -- -- -- -- -- --
 
 {config, pkgs, ...}: {
-  
-  boot.kernelParams = [ "quiet" ];
-  boot.consoleLogLevel = 3;
-  
+
   imports = [
     ../../_shared/boot/amd.nix
     ../../_shared/boot/plymouth.nix
     ./loader.nix
   ];
+
+  boot = {
+    kernelParams = [ "quiet" ];
+    extraModulePackages = [ config.boot.kernelPackages.ddcci-driver ];
+    kernelModules = ["i2c_dev" "ddcci_backlight"];  #! MSI Monitor, Model:G2712
+    consoleLogLevel = 3;
+  };
 }
