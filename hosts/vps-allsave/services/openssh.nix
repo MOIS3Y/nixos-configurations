1697232f0da85b6ad8658a41f2584c89d5020714
MOIS3Y <stepan@zhukovsky.me>
2024-06-30 11:52:44 +0900

    Add: sshd config to allsave

--- hosts/vps-allsave/services/openssh.nix
+++ hosts/vps-allsave/services/openssh.nix
@@ -6,5 +6,21 @@
   services.openssh = {
     enable = true;
     startWhenNeeded = true;
+    allowSFTP = true;
+    settings = {
+      PermitRootLogin = "yes";  # TODO: disable after enable sops
+      PasswordAuthentication = true;  # TODO: disable after enable sops
+      LogLevel = "INFO";
+    };
+    listenAddresses = [
+      {
+        addr = "0.0.0.0";
+        port = 22;  # default
+      }
+    ];
+    banner = ''
+      ▄▀█ █░░ █░░ █▀ ▄▀█ █░█ █▀▀
+      █▀█ █▄▄ █▄▄ ▄█ █▀█ ▀▄▀ ██▄
+    '';
   };
 }
