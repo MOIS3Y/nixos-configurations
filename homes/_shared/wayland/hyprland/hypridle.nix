dd64d881ff71d4879c2c7e6fec394ba865009e55
MOIS3Y <stepan@zhukovsky.me>
2024-07-10 00:01:44 +0900

    Add: hypridle notification script

--- homes/_shared/wayland/hyprland/hypridle.nix
+++ homes/_shared/wayland/hyprland/hypridle.nix
@@ -4,6 +4,18 @@
 
 { inputs, config, pkgs, ... }:
   let
+    # bin tools:
+    hyprctl = "${pkgs.hyprland}/bin/hyprctl";
+    hyprlock = "${pkgs.hyprlock}/bin/hyprlock";
+    notify-send = "${pkgs.libnotify}/bin/notify-send";
+    paplay = "${pkgs.pulseaudio}/bin/paplay";
+    pgrep = "${pkgs.procps}/bin/pgrep";
+    pkill = "${pkgs.procps}/bin/pkill";
+    seq = "${pkgs.coreutils}/bin/seq";
+    sleep = "${pkgs.coreutils}/bin/sleep";
+    swaylock = "${pkgs.swaylock-effects}/bin/swaylock";
+
+    # scripts:
     hypridle-suspend = with pkgs; writeShellScript "hypridle-suspend" ''
       # TODO: add func use pipwire
       # ${pipewire}/bin/pw-cli i all 2>&1 | ${ripgrep}/bin/rg running -q
@@ -14,11 +26,47 @@
         ${systemd}/bin/systemctl suspend
       fi
     '';
-    pgrep = "${pkgs.procps}/bin/pgrep";
-    notify-send = "${pkgs.libnotify}/bin/notify-send";
-    swaylock = "${pkgs.swaylock-effects}/bin/swaylock";
-    hyprctl = "${pkgs.hyprland}/bin/hyprctl";
-    hyprlock = "${pkgs.hyprlock}/bin/hyprlock";
+    hypridle-notify = with pkgs; writeShellScript "hypridle-notify" ''
+      print_msg() {
+        printf "\n"
+        printf "The device will soon \n"
+        printf "be blocked due to inactivity!"
+      }
+
+      idle_msg() {
+        ${seq} 100 | while read -r number; do
+          ${notify-send} \
+            -a Hyprland \
+            -i display \
+            -h int:value:"$number" \
+            -h string:synchronous:my-progress "$(print_msg)" \
+            -t 888
+          ${sleep} 0.6  # around 1 minute
+        done
+      }
+
+      main() {
+        ${paplay} ${pkgs.extrapkgs.assets4nix}/share/sounds/alarm/answer-quickly.mp3
+        idle_msg
+      }
+
+      # RUN IT:
+      main
+    '';
+    hypridle-notify-killer = with pkgs; writeShellScript "hypridle-notify-killer" ''
+      killer() {
+        if ${pgrep} -f "${hypridle-notify}" > /dev/null; then
+          ${pkill} -f "${hypridle-notify}"
+        fi
+      }
+
+      main() {
+        killer
+      }
+
+      # RUN IT:
+      main
+    '';
   in {
   services.hypridle = {
     enable = true;
@@ -34,8 +82,8 @@
       listener = [
         {
           timeout = 600;
-          on-timeout = "${notify-send} 'The device will soon be blocked due to inactivity!'";
-          onResume = "${notify-send} 'Welcome back!'";
+          on-timeout = "${hypridle-notify}";
+          on-resume = "${hypridle-notify-killer}";
         }
         {
           timeout = 660;
