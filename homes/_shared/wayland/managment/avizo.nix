3e42ea32248d71510e7d0dbe2109f872cf121694
MOIS3Y <stepan@zhukovsky.me>
2024-07-09 12:54:52 +0900

    Update: avizo icons follow system color scheme

--- homes/_shared/wayland/managment/avizo.nix
+++ homes/_shared/wayland/managment/avizo.nix
@@ -5,6 +5,13 @@
 { config, pkgs, ... }: {
   services.avizo = {
     enable = true;
+    # ? workaround avizo ignore image-base-dir
+    # ? see: https://github.com/heyjuvi/avizo/issues/60
+    package = pkgs.avizo.overrideAttrs (final: prev: {
+      patchPhase = ''
+        cp ${pkgs.extrapkgs.assets4nix}/share/icons/avizo/${config.colorScheme.name}/* data/images/
+      '';
+    });
     settings = {
       default = {
         # image-opacity=DOUBLE;
@@ -22,10 +29,11 @@
         fade-out = "0.5";
         background = "#${config.colorScheme.palette.base00}";
         border-color = "#${config.colorScheme.palette.base01}";
-        bar-fg-color = "#${config.colorScheme.palette.base0E}";
-        bar-bg-color = "#${config.colorScheme.palette.base01}";
+        bar-fg-color = "#${config.colorScheme.palette.base05}";
+        bar-bg-color = "#${config.colorScheme.palette.base02}";
         time = 2;
         # monitor = INT;
+        #image-base-dir = "${pkgs.extrapkgs.assets4nix}/share/icons/avizo/${config.colorScheme.name}";
       };
     };
   };
