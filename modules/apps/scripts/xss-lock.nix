69f642668c5eb9b4b71beb38d9f351b58b5a155f
MOIS3Y <stepan@zhukovsky.me>
2024-07-13 19:20:38 +0900

    Fix: xss-lock cmd

--- modules/apps/scripts/xss-lock.nix
+++ modules/apps/scripts/xss-lock.nix
@@ -4,9 +4,8 @@
 
 { config, pkgs, utils, ... }: with utils; {
   lock = ''
-    ${i3lock-run} \
-    -s ${config.colorScheme.name} \
-    -f Ubuntu
+    ${xss-lock} --session ''${XDG_SESSION_ID} \
+    -- ${i3lock-run} -s ${config.colorScheme.name} -f Ubuntu
   '';
   dpms-off = "${xset} s off s noblank -dpms";
 }
