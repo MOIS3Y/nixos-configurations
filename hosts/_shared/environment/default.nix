9f05f9383f736186ac0d6cf3c70fca0e21f1b58b
MOIS3Y <stepan@zhukovsky.me>
2024-07-18 21:13:31 +0900

    Update: neofetch works again

--- hosts/_shared/environment/default.nix
+++ hosts/_shared/environment/default.nix
@@ -25,10 +25,7 @@
       jq
       lm_sensors
       ncdu
-      # ! neofetch breaks the build since Jul 10 2024
-      # ? see: https://discourse.nixos.org/t/error-nose-1-3-7-not-supported-for-interpreter-python3-12/48703/9
-      # ? and https://github.com/NixOS/nixpkgs/issues/325657
-      # neofetch
+      neofetch
       nitch
       nmap
       ntfs3g
