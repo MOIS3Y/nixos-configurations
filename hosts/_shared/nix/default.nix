1744eb12775f377423d63456b06d1c71137d3bd6
MOIS3Y <stepan@zhukovsky.me>
2025-02-18 12:55:33 +0900

    chore: remove hypr overlays

--- hosts/_shared/nix/default.nix
+++ hosts/_shared/nix/default.nix
@@ -9,9 +9,7 @@
       experimental-features = nix-command flakes
     '';
     settings = {
-      trusted-users = [ "stepan" ];
-      substituters = ["https://hyprland.cachix.org"];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+      trusted-users = [ "@wheel" ];
     };
   };
 }
--- hosts/_shared/nixpkgs/default.nix
+++ hosts/_shared/nixpkgs/default.nix
@@ -10,19 +10,14 @@
     overlays = [
       (final: prev:{
         extra = {
-          # Xorg stuff:
+          # Xorg utils:
           i3lock-run = inputs.i3lock-color-wrapper.packages.${pkgs.stdenv.hostPlatform.system}.i3lock-color-wrapper;
           xidlehook-caffeine = inputs.xidlehook-caffeine.packages.${pkgs.stdenv.hostPlatform.system}.xidlehook-caffeine;
-          # Common stuff;
+          # Common utils;
           aladdin4nix = inputs.aladdin4nix.packages.${pkgs.stdenv.hostPlatform.system}.aladdin4nix;
           assets4nix = inputs.assets4nix.packages.${pkgs.stdenv.hostPlatform.system}.assets4nix;
           sddm-sugar-candy = inputs.sddmSugarCandy4Nix.packages.${pkgs.stdenv.hostPlatform.system}.sddm-sugar-candy;
           nvchad = inputs.nvchad4nix.packages.${pkgs.stdenv.hostPlatform.system}.nvchad;
-          # Hypr stuff:
-          hyprland = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-          xdg-desktop-portal-hyprland = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-          hyprlock = inputs.hyprlock.packages.${pkgs.stdenv.hostPlatform.system}.hyprlock;
-          hyprsplit = inputs.hyprsplit.packages.${pkgs.stdenv.hostPlatform.system}.hyprsplit;
         };
       })
     ];
