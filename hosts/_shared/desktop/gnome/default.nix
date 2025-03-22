c1ac83f7bea5ee420681837b235951bbf3387b87
MOIS3Y <stepan@zhukovsky.me>
2025-03-22 23:19:27 +0900

    add: evince

--- hosts/_shared/desktop/gnome/default.nix
+++ hosts/_shared/desktop/gnome/default.nix
@@ -29,6 +29,7 @@
   environment.systemPackages = with pkgs; [
     at-spi2-atk  # required for polkit-gnome-authentication-agent-1
     adwaita-icon-theme  # required for most gnome apps
+    evince
     glib  # required for gnome-weather
     nautilus
     gnome-calculator
