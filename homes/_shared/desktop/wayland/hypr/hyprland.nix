11c64fdce5151b04b4ca3b2a064787b2b77a9087
MOIS3Y <stepan@zhukovsky.me>
2025-04-05 00:27:04 +0900

    chore: switch to using inherit (expr) attr and let in instead of with

--- homes/_shared/desktop/wayland/hypr/hyprland.nix
+++ homes/_shared/desktop/wayland/hypr/hyprland.nix
@@ -2,28 +2,37 @@
 # █▀█ ░█░ █▀▀ █▀▄ █▄▄ █▀█ █░▀█ █▄▀ ▄
 # -- -- -- -- -- -- -- -- -- -- -- -
 
-{ config, pkgs, lib, ... }: lib.mkIf config.desktop.wayland.enable {
+{ config, pkgs, lib, ... }: let
+  cfg = config.desktop.wayland;
+  inherit (config.colorScheme)
+    palette;
+  inherit (config.desktop)
+    apps
+    devices
+    scripts;
+  in {
   wayland.windowManager.hyprland = {
-    enable = true;
+    enable = cfg.enable;
     package = pkgs.hyprland;
     plugins = [
       pkgs.hyprlandPlugins.hyprsplit
+      pkgs.hyprlandPlugins.hyprexpo
     ];
     xwayland.enable = true;
     settings = {
       #! -- -- -- -- -- -- autostart -- -- -- -- -- -- #
-      exec-once = "${config.desktop.scripts.hyprland.startup}";
+      exec-once = "${scripts.hyprland.startup}";
       #! -- -- -- -- -- -- modifiers -- -- -- -- -- -- #
       "$mod" = "SUPER";
       #! -- -- -- --  -- -- apps -- -- -- -- -- -- --  #
-      "$terminal" = "${config.desktop.apps.terminal}";
-      "$spare-terminal" = "${config.desktop.apps.spare-terminal}";
-      "$launcher" = "${config.desktop.scripts.hyprland.launcher-toggle}";
-      "$browser" = "${config.desktop.apps.browser}";
-      "$filemanager" = "${config.desktop.apps.filemanager}";
-      "$visual-text-editor" = "${config.desktop.apps.visual-text-editor}";
-      "$screenshot" = "${config.desktop.scripts.hyprland.screenshot}";
-      "$lockscreen" = "${config.desktop.apps.lockscreen}";
+      "$terminal" = "${apps.terminal}";
+      "$spare-terminal" = "${apps.spare-terminal}";
+      "$launcher" = "${scripts.hyprland.launcher-toggle}";
+      "$browser" = "${apps.browser}";
+      "$filemanager" = "${apps.filemanager}";
+      "$visual-text-editor" = "${apps.visual-text-editor}";
+      "$screenshot" = "${scripts.hyprland.screenshot}";
+      "$lockscreen" = "${apps.lockscreen}";
       #! -- -- -- --  -- -- environment -- -- -- -- -- #
       env = [
         "WLR_NO_HARDWARE_CURSORS,1"  # ? If your cursor becomes invisible
@@ -44,20 +53,20 @@
         in
           "${m.name},${if m.enabled then "${resolution},${position},1" else "disable"}"
         )
-        (config.desktop.devices.monitors)) ++ [ ",preferred,auto,1" ];
+        (devices.monitors)) ++ [ ",preferred,auto,1" ];
       #! -- -- -- --  -- -- workspace -- -- -- -- -- -- #
       # Currently using the hyprsplit plugin
       # I don't set any specific behavior for specific workspaces,
       # they are all configured the same
       # workspace = [ ];
       #! -- -- -- --  -- -- general -- -- -- -- -- --  #
-      general = with config.colorScheme.palette; {
+      general = {
         border_size = 1;
         no_border_on_floating = false;
         gaps_in = 4;
         gaps_out = 8;
-        "col.inactive_border" = "rgba(${base00}ff)";
-        "col.active_border" = "rgba(${base0D}ff) rgba(${base0E}ff) 60deg";
+        "col.inactive_border" = "rgba(${palette.base00}ff)";
+        "col.active_border" = "rgba(${palette.base0D}ff) rgba(${palette.base0E}ff) 60deg";
         resize_on_border = true;
       };
       #! -- -- -- --  -- -- decoration -- -- -- -- --  #
@@ -73,10 +82,10 @@
         };
       };
       #! -- -- -- --  -- -- input -- -- -- -- -- -- #
-      input = with config.desktop.devices.keyboard; {
-        kb_layout = kb_layout;
-        kb_model = kb_model;
-        kb_options = kb_options;
+      input = {
+        kb_layout = devices.keyboard.kb_layout;
+        kb_model = devices.keyboard.kb_model;
+        kb_options = devices.keyboard.kb_options;
         numlock_by_default = true;
         follow_mouse = 1;
         touchpad = {
@@ -131,98 +140,95 @@
         # "ignorezero, swaync-notification-window"
       ];
       #! -- -- -- -- -- keybindings -- -- -- -- -- #
-      bind = with config.desktop.scripts.hyprland; let
-          # first monitor in the list is primary by default:
-          pm = builtins.elemAt config.desktop.devices.monitors 0;
-          resolution = "${toString pm.width}x${toString pm.height}@${toString pm.refreshRate}";
-          position = "${toString pm.x}x${toString pm.y}";
-        in [
-          # ------------ #
-          #  - GENERAL - #
-          # ------------ #
-          "CTRL ALT, L, exec, $lockscreen"
-          # ------------ #
-          #  - WINDOWS - #
-          # ------------ # 
-          "$mod, w,   killactive"
-          "$mod, TAB, togglefloating"
-          "$mod  SHIFT, TAB, fullscreen"
-          # move focus with mod
-          "$mod, h, movefocus, l"
-          "$mod, j, movefocus, d"
-          "$mod, k, movefocus, u"
-          "$mod, l, movefocus, r"
-          # move focus to urgent window
-          "$mod, u, focusurgentorlast"
-          # move focus to next window
-          "$mod, space, cyclenext"
-          # move window position
-          "$mod SHIFT, h, movewindow, l"
-          "$mod SHIFT, j, movewindow, d"
-          "$mod SHIFT, k, movewindow, u"
-          "$mod SHIFT, l, movewindow, r"
-          # resize active window
-          "$mod CTRL,  h, resizeactive, -20 0"
-          "$mod CTRL,  j, resizeactive, 0 20"
-          "$mod CTRL,  k, resizeactive, 0 -20"
-          "$mod CTRL,  l, resizeactive, 20 0"
-          # hyprsplit:
-          "$mod, G, split:grabroguewindows"
-          # ---------- #
-          #  - APPS -  #
-          # ---------- # 
-          "$mod, RETURN, exec, $terminal"
-          "$mod, t,      exec, $spare-terminal"
-          "$mod, m,      exec, $launcher"
-          "$mod, b,      exec, $browser"
-          "$mod, f,      exec, $filemanager"
-          "$mod, v,      exec, $visual-text-editor"
-          ",     Print,  exec, $screenshot"
-          # --------------- #
-          #  - WORKSPACES - #
-          # --------------- # 
-          # scroll through existing workspaces with $mod + scroll
-          "$mod, mouse_down, workspace, e+1"
-          "$mod, mouse_up,   workspace, e-1"
-          # --------------- #
-          #  - HARDWARE -   #
-          # --------------- #
-          # sound and brightness managment
-          ",XF86MonBrightnessUp,   exec, ${brightness-up}"
-          ",XF86MonBrightnessDown, exec, ${brightness-down}"
-          ",XF86AudioRaiseVolume,  exec, ${volume-up}"
-          ",XF86AudioLowerVolume,  exec, ${volume-down}"
-          ",XF86AudioMute,         exec, ${volume-mute}"
-          ",XF86AudioMicMute,      exec, ${mic-mute}"
-          # --------------- #
-          #  - SWITCHES -   #
-          # --------------- #
-          # ! broken see: https://github.com/hyprwm/Hyprland/issues/7055
-          # '',switch:on:Lid Switch,exec,hyprctl keyword monitor "${pm.name},disable"''
-          # '',switch:off:Lid Switch,exec,hyprctl keyword monitor "${pm.name},${resolution},${position},1"''
-        ]
-        ++ (
-          # ----------------- #
-          #  - WORKSPACES -   #
-          # ----------------- #
-          # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-          builtins.concatLists (builtins.genList (
-              x: let
-                ws = let
-                  c = (x + 1) / 10;
-                in
-                  builtins.toString (x + 1 - (c * 10));
-              in [
-                # Default:
-                # "$mod,        ${ws}, focusworkspaceoncurrentmonitor, ${toString (x + 1)}"
-                # "$mod  SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-                # Hyprsplit:
-                "$mod,        ${ws}, split:workspace,       ${toString (x + 1)}"
-                "$mod  SHIFT, ${ws}, split:movetoworkspace, ${toString (x + 1)}"
-              ]
-            )
-            10)
-        );
+      bind = let
+        # first monitor in the list is primary by default:
+        pm = builtins.elemAt devices.monitors 0;
+        resolution = "${toString pm.width}x${toString pm.height}@${toString pm.refreshRate}";
+        position = "${toString pm.x}x${toString pm.y}";
+      in [
+        # ------------ #
+        #  - GENERAL - #
+        # ------------ #
+        "CTRL ALT, L, exec, $lockscreen"
+        # ------------ #
+        #  - WINDOWS - #
+        # ------------ # 
+        "$mod, w,   killactive"
+        "$mod, TAB, togglefloating"
+        "$mod  SHIFT, TAB, fullscreen"
+        # move focus with mod
+        "$mod, h, movefocus, l"
+        "$mod, j, movefocus, d"
+        "$mod, k, movefocus, u"
+        "$mod, l, movefocus, r"
+        # move focus to urgent window
+        "$mod, u, focusurgentorlast"
+        # move focus to next window
+        "$mod, space, cyclenext"
+        # move window position
+        "$mod SHIFT, h, movewindow, l"
+        "$mod SHIFT, j, movewindow, d"
+        "$mod SHIFT, k, movewindow, u"
+        "$mod SHIFT, l, movewindow, r"
+        # resize active window
+        "$mod CTRL,  h, resizeactive, -20 0"
+        "$mod CTRL,  j, resizeactive, 0 20"
+        "$mod CTRL,  k, resizeactive, 0 -20"
+        "$mod CTRL,  l, resizeactive, 20 0"
+        # hyprsplit:
+        "$mod, G, split:grabroguewindows"
+        # ---------- #
+        #  - APPS -  #
+        # ---------- # 
+        "$mod, RETURN, exec, $terminal"
+        "$mod, t,      exec, $spare-terminal"
+        "$mod, m,      exec, $launcher"
+        "$mod, b,      exec, $browser"
+        "$mod, f,      exec, $filemanager"
+        "$mod, v,      exec, $visual-text-editor"
+        ",     Print,  exec, $screenshot"
+        # --------------- #
+        #  - WORKSPACES - #
+        # --------------- # 
+        # scroll through existing workspaces with $mod + scroll
+        "$mod, mouse_down, workspace, e+1"
+        "$mod, mouse_up,   workspace, e-1"
+        # --------------- #
+        #  - SWITCHES -   #
+        # --------------- #
+        # ! broken see: https://github.com/hyprwm/Hyprland/issues/7055
+        # '',switch:on:Lid Switch,exec,hyprctl keyword monitor "${pm.name},disable"''
+        # '',switch:off:Lid Switch,exec,hyprctl keyword monitor "${pm.name},${resolution},${position},1"''
+      ]
+      ++ (
+        # ----------------- #
+        #  - WORKSPACES -   #
+        # ----------------- #
+        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+        builtins.concatLists (builtins.genList (
+            x: let
+              ws = let
+                c = (x + 1) / 10;
+              in
+                builtins.toString (x + 1 - (c * 10));
+            in [
+              # Default:
+              # "$mod,        ${ws}, focusworkspaceoncurrentmonitor, ${toString (x + 1)}"
+              # "$mod  SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+              # Hyprsplit:
+              "$mod,        ${ws}, split:workspace,       ${toString (x + 1)}"
+              "$mod  SHIFT, ${ws}, split:movetoworkspace, ${toString (x + 1)}"
+            ]
+          )
+          10)
+      );
+      bindr = [
+        # ----------------- #
+        #  - WORKSPACES -   #
+        # ----------------- #
+        # Hyprexpo:
+        "$mod, SUPER_L, hyprexpo:expo, toggle"
+      ];
       bindm = [
         # ------------ #
         #  - WINDOWS - #
@@ -230,6 +236,18 @@
         # move and resize window
         "$mod, mouse:272, movewindow"
         "$mod, mouse:273, resizewindow"
+      ];
+      bindle = [
+        # --------------- #
+        #  - HARDWARE -   #
+        # --------------- #
+        # sound and brightness managment
+        ",XF86MonBrightnessUp,   exec, ${scripts.hyprland.brightness-up}"
+        ",XF86MonBrightnessDown, exec, ${scripts.hyprland.brightness-down}"
+        ",XF86AudioRaiseVolume,  exec, ${scripts.hyprland.volume-up}"
+        ",XF86AudioLowerVolume,  exec, ${scripts.hyprland.volume-down}"
+        ",XF86AudioMute,         exec, ${scripts.hyprland.volume-mute}"
+        ",XF86AudioMicMute,      exec, ${scripts.hyprland.mic-mute}"
       ];
       #! -- -- -- -- -- xwayland -- -- -- -- -- #
       xwayland = {
@@ -239,9 +257,21 @@
     #! -- -- -- -- -- extra config -- -- -- -- #
     extraConfig = ''
       plugin {
-          hyprsplit {
-              num_workspaces = 10
-          }
+        # ? https://github.com/shezdy/hyprsplit/blob/main/README.md
+        hyprsplit {
+            num_workspaces = 10
+        }
+        # ? https://github.com/hyprwm/hyprland-plugins/tree/main/hyprexpo#readme
+        hyprexpo {
+          columns = 3
+          gap_size = 10
+          bg_col = rgb(${palette.base01})
+          workspace_method = center current
+          enable_gesture = true
+          gesture_fingers = 3
+          gesture_distance = 300
+          gesture_positive = true
+        }
       }
     '';
   };
