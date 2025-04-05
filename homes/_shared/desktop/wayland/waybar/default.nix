04d270c41f0a01874f9016bf4df2f88fe8b62620
MOIS3Y <stepan@zhukovsky.me>
2025-04-05 22:15:18 +0900

    chore: squash waybar module

--- homes/_shared/desktop/wayland/waybar/default.nix
+++ homes/_shared/desktop/wayland/waybar/default.nix
@@ -4,18 +4,75 @@
 
 { config, pkgs, lib, ... }: let
   cfg = config.desktop.wayland;
+  bars = import ./bars.nix { inherit config lib; };
+  styles = import ./styles.nix { inherit config;};
+  inherit (lib)
+    mkOption
+    types;
   in {
-  imports = [
-    ./waybar.nix
-  ];
-  programs.waybar = {
-    enable = cfg.enable;
-    systemd.enable = true;
-    excludeWidgets = if config.desktop.laptop.enable
-      then [ "custom/ddcutil" ]
-      else [ "battery" "group/group-backlight" ];
+  options.programs.waybar = {
+    excludeWidgets = mkOption {
+      default = [];
+      type = with types; listOf (enum [
+        "battery"
+        "group/group-backlight"
+        "custom/ddcutil"
+      ]);
+      description = "Exclude widgets from current bar.";
+    };
     ddcutil = {
-      busNumber = 6;
+      busNumber = mkOption {
+        default = 6;  # ? my monitor on /sys/bus/i2c/devices/i2c-6 card1-DP-1
+        type = types.number;
+        description = ''
+          The bus number is the DP or HDMI port of the video card 
+          to which the monitor is connected.
+        '';
+      };
+      multiplier = mkOption {
+        default = 5;
+        type = types.enum [ 1 2 3 4 5 6 7 ];
+        description = ''
+          Required for speed of operation and reduction of imput lag.
+        '';
+      };
+      step = mkOption {
+        default = 5;
+        type = types.number;
+        description = ''
+          Steps to increase or decrease brightness.
+        '';
+      };
+      bright = mkOption {
+        default = 50;
+        type = types.number;
+        description = ''
+          Quickly set screen brightness to a specified value.
+        '';
+      };
+      dark = mkOption {
+        default = 25;
+        type = types.number;
+        description = ''
+          Quickly set screen brightness to a specified value.
+        '';
+      };
+    };
+  };
+  config = {
+    programs.waybar = {
+      enable = cfg.enable;
+      systemd.enable = true;
+      excludeWidgets = if config.desktop.laptop.enable
+        then [ "custom/ddcutil" ]
+        else [ "battery" "group/group-backlight" ];
+      ddcutil = {
+        busNumber = 6;
+      };
+      settings = {
+        primaryBar = bars.primary;
+      };
+      style = styles.primary;
     };
   };
 }
