ec7ab055324e558002fc667961dfd1a52f96cc15
MOIS3Y <stepan@zhukovsky.me>
2025-04-21 00:30:09 +0900

    add: retroarch with nestopia core

--- modules/desktop/workstation.nix
+++ modules/desktop/workstation.nix
@@ -32,6 +32,9 @@
       };
       extraPackages = [
         pkgs.bottles
+        (pkgs.retroarch.withCores (cores: with cores; [
+          nestopia
+        ]))
       ];
     };
     devices = {
