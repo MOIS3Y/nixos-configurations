f7951f69061d2ccc0f550730d6b6ebe648781fc2
MOIS3Y <stepan@zhukovsky.me>
2025-05-19 08:51:47 +0900

    update: nixpkgs input

--- homes/_shared/desktop/pkgs/media.nix
+++ homes/_shared/desktop/pkgs/media.nix
@@ -8,9 +8,7 @@
     cassette
     celluloid
     delfin
-    #! broken build  electron-33.4.11 is EOL
-    #? see https://github.com/NixOS/nixpkgs/issues/404494
-    # feishin 
+    feishin 
     imv
     shotwell
     transmission_4-gtk
