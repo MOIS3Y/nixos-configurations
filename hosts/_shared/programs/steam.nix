5ce591cbe1e508e7bc3abf41cb84bba1baaf7e53
MOIS3Y <stepan@zhukovsky.me>
2025-07-31 00:35:25 +0900

    chore: clear steam module remove lib.mkIf

--- hosts/_shared/programs/steam.nix
+++ hosts/_shared/programs/steam.nix
@@ -2,10 +2,10 @@
 # ▄█ ░█░ ██▄ █▀█ █░▀░█ ▄
 # -- -- -- -- -- -- -- -
 
-{ config, lib, ... }: {
+{ config, ... }: {
   programs.steam = {
-    enable = lib.mkIf config.desktop.games.enable true;
-    extest.enable = lib.mkIf config.desktop.wayland.enable true;
+    enable = config.desktop.games.enable;
+    extest.enable = config.desktop.wayland.enable;
     gamescopeSession = {
       enable = true;
     };
