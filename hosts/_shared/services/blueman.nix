45d9f92929a2c24a26d1602d1af0ee079786fcb1
MOIS3Y <stepan@zhukovsky.me>
2025-08-03 15:55:28 +0900

    chore(gnome): add enable check

--- hosts/_shared/services/blueman.nix
+++ hosts/_shared/services/blueman.nix
@@ -6,6 +6,7 @@
   # ! required for blueman-applet HM-systemd-service (hide error msg)
   services.blueman.enable = (
     config.desktop.enable &&
-    config.desktop.devices.bluetooth.enable
+    config.desktop.devices.bluetooth.enable &&
+    !config.services.desktopManager.gnome.enable
   );
 }
