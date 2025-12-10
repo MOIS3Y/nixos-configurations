dc54680dcc6f5a94b8710d3cf74b5f0cca9e37ff
MOIS3Y <stepan@zhukovsky.me>
2025-12-10 21:58:02 +0900

    refactor(pkgs): move security tools to separate module
    - Remove globally installed development packages (vault, aladdin4nix)
      as they are now managed as dependencies by respective applications
    - Create new security.nix module for frequently used security tools
    - Add age, sops, totp-cli as global packages for daily use

--- homes/_shared/pkgs/default.nix
+++ homes/_shared/pkgs/default.nix
@@ -7,6 +7,7 @@
     ./development.nix
     ./media.nix
     ./network.nix
+    ./security.nix
     ./society.nix
     ./wine.nix
     ./x11.nix
--- homes/_shared/pkgs/development.nix
+++ homes/_shared/pkgs/development.nix
@@ -13,12 +13,8 @@
     sassc
     sqlitebrowser
     uv
-    # vault  #! local building
-    vault-bin  #! In Russia, loading is available only with VPN
     # Languages:
     (lua.withPackages(ps: with ps; [ luarocks ]))
     (python3.withPackages(ps: with ps; [ pip ]))
-    # Extra-pkgs:
-    extra.aladdin4nix
   ];
 }
