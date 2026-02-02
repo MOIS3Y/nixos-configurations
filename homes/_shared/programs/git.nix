f5d29a2c816fb7bca0858ed8499b69c15adfed25
MOIS3Y <stepan@zhukovsky.me>
2026-02-02 10:44:45 +0900

    chore(git): add global ignores

--- homes/_shared/programs/git.nix
+++ homes/_shared/programs/git.nix
@@ -10,6 +10,39 @@
       key = "F40E8485FB87A653";
       signByDefault = true;
     };
+    ignores = [
+      # -- Nix & Direnv --
+      "result"
+      "result-*"
+      ".direnv/"
+
+      # -- Neovim / Vim --
+      "*.swp"
+      "*.swo"
+      "undo/"
+      ".netrwhist"
+      "Session.vim"
+
+      # -- Helix --
+      ".helix/config.toml"
+      ".helix/ignore"
+
+      # -- VS Code --
+      ".vscode/*"
+      "!.vscode/extensions.json"
+      "!.vscode/launch.json"
+      "!.vscode/settings.json"
+      "*.code-workspace"
+
+      # -- Zed --
+      ".zed/"
+      ".zed-settings.json"
+
+      # -- Gemini CLI & AI Context --
+      "/GEMINI.md"
+      ".geminiignore"
+      ".gemini/"
+    ];
     settings = {
       core.editor = "nvim";
       user = {
